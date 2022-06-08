21635ffa59830f43f1b04b2900145f40a8d800b6
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-06-08 13:01:45 +0430

    Initial Commit

---
+++ WhatsNewSwiftUI4/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WhatsNewSwiftUI4
+//
+//  Created by Ali Aghamirbabaei on 6/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhatsNewSwiftUI4/WhatsNewSwiftUI4App.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsNewSwiftUI4App.swift
+//  WhatsNewSwiftUI4
+//
+//  Created by Ali Aghamirbabaei on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct WhatsNewSwiftUI4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
